03d10bb4fd607193db1cd326bd644225110b92a9
YodaLightsabr <yoda@yodacode.xyz>
2022-07-12 02:53:02 +0000

    Update gh-pages to output generated at db74c8f

---
+++ _next/static/lLQ-D93vsE18zAjwUyeFp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/996-7263fe363f67a2c0.js","static/chunks/pages/index-bfce6a0f46280e42.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
