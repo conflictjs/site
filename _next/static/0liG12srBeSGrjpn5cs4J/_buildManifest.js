1df87329f7469b5a2ad24821f15655673b56da59
YodaLightsabr <yoda@yodacode.xyz>
2022-07-27 04:25:42 +0000

    Update gh-pages to output generated at d1dba7b

---
+++ _next/static/0liG12srBeSGrjpn5cs4J/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/996-508814179ea1041c.js","static/chunks/pages/index-bfce6a0f46280e42.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
