61c197b21d713711d0e72df1b6de3d70c9f1b792
YodaLightsabr <yoda@yodacode.xyz>
2022-09-26 23:01:52 +0000

    Update gh-pages to output generated at 48bf6e4

---
+++ _next/static/HdXlrfv4hKBpfMhv0vonq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/996-aceb83aa6d5b0ab8.js","static/chunks/pages/index-bff9e68a7b8c6f95.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/VscOD3ZDYtt8MThMg4Q2L/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/996-9dcc742b16361397.js","static/chunks/pages/index-bff9e68a7b8c6f95.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
